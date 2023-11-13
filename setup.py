7cc14e37c9b9f9958ad7ac157c31705fae918f58
VINOTH KUMAR R <vinoth.mila@gmail.com>
2023-11-13 12:10:16 +0100

    pip installation package files created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="snowflake",
+    version="0.1",
+    author="DSSS",
+    author_email="luisa.e.neubig@fau.de",
+    packages=find_packages()
+)
