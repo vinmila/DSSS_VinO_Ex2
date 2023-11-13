60a7648e769c6e0ae556a78a2dace1f6c1c10134
VINOTH KUMAR R <vinoth.mila@gmail.com>
2023-11-13 12:50:42 +0100

    package name updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name="mathsquiz",
+    name="math_quiz",
     version="0.1",
     author="Vinoth Kumar",
     author_email="vinoth.mila@gmail.com",
